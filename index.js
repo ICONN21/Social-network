a179b7d1351e3d770c03b4128eebeded788ec01e
Ian Connor <Ian.connor0921@gmail.com>
2024-08-21 15:08:16 -0400

    set up basic file structure and added some routes as well as initializing the server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const db = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Import routes
+const routes = require('./routes');
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`🌍 Connected on localhost:${PORT}`);
+  });
+});
